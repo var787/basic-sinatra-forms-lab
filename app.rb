2977d660b30404cb3883ca9a1b8766e29b0f6b51
var787 <hegde.rakshan@gmail.com>
2020-11-17 13:44:16 -0600

    commit tree 9a09d996f0b8fe4e758a79c06503024ff051309d

--- app.rb
+++ app.rb
@@ -2,5 +2,19 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        
+        
+        erb :newteam
+    
+    end
+
+    post '/team' do 
+        
+        
+        erb :team
+    
+    end
+
 
 end
